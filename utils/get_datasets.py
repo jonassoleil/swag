63686fe5b89d6282f0849386d005aee3058c5d5c
Janek <jan.piotr.latko@gmail.com>
2021-02-25 19:22:20 +0100

    fix get_datasets

--- utils/get_datasets.py
+++ utils/get_datasets.py
@@ -5,14 +5,14 @@ def get_datasets(name, batch_size_train=256, batch_size_test=1024):
     # TODO: validation
     dataset = getattr(datasets, name)
     train_loader = torch.utils.data.DataLoader(
-        dataset(f'{name.lower}_data', train=True, download=True,
+        dataset(f'{name.lower()}_data', train=True, download=True,
                     transform=transforms.Compose([
                         transforms.ToTensor()
                     ])),
         batch_size=batch_size_train, shuffle=True)
 
     test_loader = torch.utils.data.DataLoader(
-        dataset(f'{name.lower}_data', train=False, transform=transforms.Compose([
+        dataset(f'{name.lower()}_data', train=False, transform=transforms.Compose([
                         transforms.ToTensor()
                     ])),
         batch_size=batch_size_test, shuffle=False)
