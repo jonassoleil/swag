b2bac376efaa212e0fad89726bd6523412bff589
Janek <jan.piotr.latko@gmail.com>
2021-03-19 00:02:16 +0100

    bugfix

--- src/utils/wandb_model_checkpoint.py
+++ src/utils/wandb_model_checkpoint.py
@@ -4,10 +4,10 @@ import pytorch_lightning as pl
 class WandBModelCheckpoint(pl.callbacks.ModelCheckpoint):
     def _save_model(self, trainer, filepath: str, pl_module=None):
         # monkey patch, support both versions
-        if 'pl_module' in super()._save_model.__code__.co_varnames:
-            super()._save_model(trainer, filepath)
-        else:
-            super()._save_model(trainer, filepath, pl_module=None)
+        # if 'pl_module' in super()._save_model.__code__.co_varnames:
+        #     super()._save_model(trainer, filepath)
+        # else:
+        super()._save_model(trainer, filepath, pl_module=pl_module)
         # TODO: make sure it works
         # print('saving for epoch: ', trainer.current_epoch)
         # print('saving: ', filepath)
