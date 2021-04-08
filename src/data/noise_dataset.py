5798e15ac33ae7510afe6626788e9b1526292c95
Janek <jan.piotr.latko@gmail.com>
2021-04-08 09:34:07 +0200

    seeded noise dataset

--- src/data/noise_dataset.py
+++ src/data/noise_dataset.py
@@ -3,16 +3,19 @@ import torch
 from src.data import BaseDataModule
 
 class NoiseDataset(torch.utils.data.Dataset):
-    def __init__(self, shape, len=2000):
+    def __init__(self, shape, len=2000, seed=0):
         self.shape = list(shape)
         self.len = len
         self.targets = torch.zeros(len)
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        self.data = torch.randn([self.len] + self.shape)
 
     def __len__(self):
         return self.len
 
     def __getitem__(self, index):
-        return torch.randn(self.shape), 0
+        return self.data[index], 0
 
 class NoiseDatasetPL(BaseDataModule):
     def __init__(self, args):
@@ -24,6 +27,6 @@ class NoiseDatasetPL(BaseDataModule):
 
     def setup(self, stage=None):
         self.data_train = NoiseDataset(self.shape)
-        self.data_val = NoiseDataset(self.shape)
-        self.data_test = NoiseDataset(self.shape)
+        self.data_val = self.data_train
+        self.data_test = self.data_train
 
