92e2f5c58f69f01c93258ed0c2f9ecbd2916793f
Janek <jan.piotr.latko@gmail.com>
2021-04-21 17:38:27 +0200

    swag iterator

--- src/modules/swag_iterator.py
+++ src/modules/swag_iterator.py
@@ -1,5 +1,9 @@
+import torch
+import numpy as np
+
 from src.modules.base_model_iterator import BaseModelIterator
-from src.utils.load_utils import get_k_last_checkpoints
+from src.modules.interpolated_iterator import get_flattened_params, flattened_to_state_dict
+from src.utils.load_utils import get_k_last_checkpoints, get_state_from_checkpoint
 from src.utils.update_bn import update_batch_normalization
 
 
@@ -14,11 +18,34 @@ class SWAGIterator(BaseModelIterator):
         self.train_loader = train_loader
         self.model = model
         self.run_id = run_id
-        # TODO: initiate weight distribution parameters
+
+        self.K = len(self.checkpoints)
+
+        checkpoint_weights_flattened = []
+        for ch in self.checkpoints:
+            w = get_state_from_checkpoint(run_id, ch)
+            wf = get_flattened_params(w)
+            checkpoint_weights_flattened.append(wf)
+
+        d = torch.stack(checkpoint_weights_flattened)
+        # compute mean
+        self.w_mean = torch.mean(d, dim=0)
+        self.D = len(self.w_mean)
+        # compute diagonal variance (and take square root)
+        self.sigma_diag = torch.sqrt(torch.abs(torch.mean(torch.square(d), dim=0) - torch.square(self.w_mean)))
+        # get low rank part
+        self.d_hat = d - self.w_mean
+        # save constants for convenience
+        self.c1 = 1 / np.sqrt(2)
+        self.c2 = 1 / np.sqrt(2 * (self.K - 1))
 
     def sample_weights(self):
-        # TODO: sample weights
-        raise NotImplementedError
+        # from eq (1) in SWAG paper
+        z1 = torch.randn(self.D)
+        z2 = torch.randn(self.K)
+        new_weights = self.w_mean + self.c1 * z1 * self.sigma_diag + self.c2 * (z2 @ self.d_hat)
+
+        return flattened_to_state_dict(new_weights, self.model.state_dict())
 
 
     def get_next_model(self):
